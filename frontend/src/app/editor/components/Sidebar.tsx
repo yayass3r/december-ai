b9edbb4ab3ecb219ec94fcab46240854dd91a329
Julian Schoen <26648900+ntegrals@users.noreply.github.com>
2025-06-08 00:23:01 +0200

    core: improve sidebar

--- frontend/src/app/editor/components/Sidebar.tsx
+++ frontend/src/app/editor/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const Sidebar = ({ children }: { children: ReactNode }) => {
   return (
-    <aside className="block w-60 h-screen border-r-2 border-neutral-800 pt-1">
+    <aside className="flex flex-col w-60 h-full border-r-2 border-neutral-800">
       {children}
     </aside>
   );
